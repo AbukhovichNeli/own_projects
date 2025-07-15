3aeb0118d00a10e57ef202dc30abadae7bb2980d
Obukhovich Nelli <obukhovichnelly@gmail.com>
2025-07-15 22:27:46 +0300

    feat: add initial layout to Search.vue

---
+++ weather-app/src/components/SearchBar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <input v-model="query" @keyup.enter="searchLocation" placeholder="Enter city name" />
+    <ul v-if="results.length">
+      <li v-for="loc in results" :key="loc.id" @click="selectLocation(loc)">
+        {{ loc.name }}, {{ loc.country }}
+      </li>
+    </ul>
+  </div>
+</template>
